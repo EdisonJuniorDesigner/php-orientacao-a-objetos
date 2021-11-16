7e32758089e2f091302804e31643d057523631b7
EdisonJuniorDesigner <mage1232010@gmail.com>
2021-11-15 23:36:22 -0300

    Classe Funcionário Abstrata, Novas Classes extendidas de Funcionário, novos métodos

--- bonificacoes.php
+++ bonificacoes.php
@@ -1,26 +1,37 @@
 <?php
 
-use Alura\Banco\Modelo\{CPF, Funcionario};
+use Alura\Banco\Modelo\{CPF};
+use Alura\Banco\Modelo\Funcionario\{Funcionario, Gerente, Diretor, Desenvolvedor};
 use Alura\Banco\Service\{ControladorDeBonificacoes};
 
 require_once 'autoload.php';
 
-$umFuncionario = new Funcionario(
+$umFuncionario = new Desenvolvedor(
     'Edison Junior',
     new CPF('123.456.789-10'),
     'Desenvolvedor',
     1000
 );
 
-$umaFuncionaria = new Funcionario(
+$umFuncionario->sobeDeNivel();
+
+$umaFuncionaria = new Gerente(
     'Patricia',
     new CPF('175.986.702-80'),
     'Gerente',
     3000
 );
 
+$umDiretor = new Diretor(
+    'Roberto',
+    new CPF('145.926.752-20'),
+    'Diretor',
+    5000
+);
+
 $controlador = new ControladorDeBonificacoes();
 $controlador->adicionaBonificacaoDe($umFuncionario);
 $controlador->adicionaBonificacaoDe($umaFuncionaria);
+$controlador->adicionaBonificacaoDe($umDiretor);
 
 echo $controlador->recuperaTotal();
